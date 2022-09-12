1337479c0d10a72abf75feaa8edd1bfda186c544
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-12 08:16:53 -0700

    Creating first route listening

---
+++ server/src/server.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Ads 1' },
+    { id: 2, name: 'Ads 2' },
+    { id: 3, name: 'Ads 3' },
+  ]);
+});
+
+app.listen(3333);
