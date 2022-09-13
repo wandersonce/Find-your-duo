750353455bd52f6ea9bab314b362757395c0a6e1
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-13 09:41:06 -0700

    Adding logo and fake images

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,37 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo.svg';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='max-w-[1344px] mx-auto flex items-center flex-col my-20'>
+      <img src={logoImg}/>
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Your <span className='text-transparent bg-clip-text bg-nlw-gradient'>Duo</span> is Here
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href="">
+          <img src="/game1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game6.png" alt="" />
+        </a>
+
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: 'url("/background-galaxy.png")',
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+      },
+    },
   },
   plugins: [],
 };
