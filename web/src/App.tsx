c0e56509b980e1274e93932cc9503b78ba99204a
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-13 11:11:16 -0700

    Adding bottom bar and icons lib

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import {MagnifyingGlassPlus} from 'phosphor-react';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo.svg';
@@ -62,6 +64,20 @@ function App() {
         </a>
 
       </div>
+
+    <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'> 
+        <div>
+        <strong className='text-2xl text-white font-black block'>Didn't find your duo?</strong>
+        <span className='text-zinc-400'>Share a post to find new players!</span>
+        </div>
+
+        <button className="py-3 px-4 bg-violet-500 text-white rounded flex items-center gap-3 hover:bg-violet-600">
+          <MagnifyingGlassPlus size={24}/>
+          Publish a Post
+        </button>
+      </div>
+    </div>
     </div>
   )
 }
