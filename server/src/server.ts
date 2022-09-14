a91c1b80f99400008cca83a0aaeb977e567327a1
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-14 12:22:24 -0700

    Routes created

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,27 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games' , (request, response) =>{
+  return response.json([]);
+})
+
+app.post('/ads' , (request, response) =>{
+  return response.json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
   return response.json([
     { id: 1, name: 'Ads 1' },
     { id: 2, name: 'Ads 2' },
     { id: 3, name: 'Ads 3' },
     { id: 4, name: 'Ads 4' },
   ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const gameId = request.params.id;
+  return response.json([]);
 });
 
 app.listen(3333);
