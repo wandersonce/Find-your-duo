83296e590e548dde74ff9a62d6d1a44a101044dc
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-14 14:29:23 -0700

    Getting data from ads and game

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,19 +24,48 @@ app.post('/ads' , (request, response) =>{
   return response.json([]);
 })
 
-app.get('/games/:id/ads', (request, response) => {
-  // const gameId = request.params.id;
-  return response.json([
-    { id: 1, name: 'Ads 1' },
-    { id: 2, name: 'Ads 2' },
-    { id: 3, name: 'Ads 3' },
-    { id: 4, name: 'Ads 4' },
-  ]);
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select:{
+      id:true,
+      name:true,
+      weekDays:true,
+      useVoiceChannel:true,
+      yearsPlaying:true,
+      hourStart:true,
+      hourEnd:true,
+    },
+    where:{
+      gameId,
+    },
+    orderBy:{
+      createdAt: 'desc'
+    }
+  })
+  return response.json(ads.map(ad => {
+    return{
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }));
 });
 
-app.get('/ads/:id/discord', (request, response) => {
-  // const gameId = request.params.id;
-  return response.json([]);
+app.get('/ads/:id/discord', async(request, response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select:{
+      discord:true,
+    },
+    where:{
+      id: adId
+    }
+  })
+  return response.json({
+    discord: ad.discord
+  });
 });
 
 app.listen(3333);
