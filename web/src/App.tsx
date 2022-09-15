264897e7b6b5cd99fc4dcd62a689fdd6ae801e4a
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-15 09:07:43 -0700

    Converting Banners to components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import {MagnifyingGlassPlus} from 'phosphor-react';
-
 import './styles/main.css';
 
 import logoImg from './assets/logo.svg';
+import GameBanner from './components/GameBanner';
+import CreateAdBanner from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -14,70 +14,10 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>Dota 2</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game2.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>CS-GO</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game3.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>APEX Legends</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game4.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>Fortnite</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game5.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>World of Warcraft</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'> 
-          <img src="/game6.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0" >
-            <strong className='font-bold text-white block '>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 Posts</span>
-          </div>
-        </a>
-
+        <GameBanner bannerUrl='/game1.png' title="League of Legends" adsCount={5} />
       </div>
 
-    <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'> 
-        <div>
-        <strong className='text-2xl text-white font-black block'>Didn't find your duo?</strong>
-        <span className='text-zinc-400'>Share a post to find new players!</span>
-        </div>
-
-        <button className="py-3 px-4 bg-violet-500 text-white rounded flex items-center gap-3 hover:bg-violet-600">
-          <MagnifyingGlassPlus size={24}/>
-          Publish a Post
-        </button>
-      </div>
-    </div>
+      <CreateAdBanner />
     </div>
   )
 }
---
+++ web/src/components/CreateAdBanner.tsx
@@ -0,0 +1,21 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
+function CreateAdBanner() {
+  return (
+    <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+    <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'> 
+      <div>
+      <strong className='text-2xl text-white font-black block'>Didn't find your duo?</strong>
+      <span className='text-zinc-400'>Share a post to find new players!</span>
+      </div>
+
+      <button className="py-3 px-4 bg-violet-500 text-white rounded flex items-center gap-3 hover:bg-violet-600">
+        <MagnifyingGlassPlus size={24}/>
+        Publish a Post
+      </button>
+    </div>
+  </div>
+  )
+}
+
+export default CreateAdBanner
